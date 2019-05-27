9afc8f34e85083befb772afb2911ff8f044fed9e
huphc1024 <leehuphc@gmail.com>
2019-05-27 22:53:58 +0700

    Make a basic User model (including secure passwords)

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,11 +1,2 @@
 module StaticPagesHelper
-  # Returns the full title on a per-page basis.
-  def full_title page_title = ""
-    base_title = I18n.t ".base_title"
-    if page_title.empty?
-      base_title
-    else
-      page_title + " | " + base_title
-    end
-  end
 end
